1b15581e96601792027d1aa433ebbe9b5b037b2c
bdas0045 <bd3838156@gmail.com>
2025-07-01 19:46:28 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",  
+SESSION_ID: process.env.SESSION_ID || "ALI-MD~SRNnGTpZ#eiyhvsnelUriPbyLJ2oxUVa0YVZHFV2pWTrASHmK4WI",  
 //chatbot integration 
 CHATBOT: process.env.CHATBOT || "on",
 // add your Session Id 
